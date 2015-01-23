ed25b75c3f06f7830adefbc2e8db8a7d1b6096bd
israel <kabokinana@walla.co.il>
2015-01-23 16:42:51 +0200

    initial

---
+++ Assets/Script/Maze.cs
@@ -0,0 +1,206 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Maze : MonoBehaviour {
+
+	//constants
+	public int MAZE_WIDTH;
+	public int MAZE_HEIGHT;
+	private const float TILE_SIZE = 1;
+	public GameObject wall;
+	//private const START_COLOR		: uint = 0xFF0000;
+	//private const FINISH_COLOR		: uint = 0x00FF00;
+	//private const WALL_COLOR		: uint = 0x000000;
+	//private const WALKABLE_COLOR	: uint = 0xDDDDDD;
+	
+	//directions
+	private const char NORTH = 'N';
+	private const char SOUTH = 'S';
+	private const char EAST = 'E';
+	private const char WEST = 'W';
+	
+	//variables
+	private int _width;
+	private int _height;
+
+	private bool[][] _maze;
+
+	private List<int> _moves;
+	private Vector2 _start;
+	private Vector2 _finish;
+	//private _container	: Sprite;
+	
+	public void TiledMazeGen ()
+	{		
+		_width	= MAZE_WIDTH * 2 + 1;
+		_height	= MAZE_HEIGHT * 2 + 1;
+
+		_start = new Vector2(1, 1);
+		//_finish = new Point(_height - 2, _width - 2);
+		
+		//_container = new Sprite();
+		
+		_generate();
+	}
+	
+	private void _generate ()
+	{
+		_initMaze();
+		_createMaze();
+		_drawMaze();
+	}
+	
+	private void _initMaze ()
+	{
+		_maze = new bool[_width][];
+		
+		for ( int x = 0; x < _height; x++ )
+		{
+			_maze[x] = new bool[_height];
+			
+			for ( int y = 0; y < _width; y++ )
+			{
+				_maze[x][y] = true;
+			}
+		}
+		
+		_maze[(int)_start.x][(int)_start.y] = false;
+	}
+	
+	private void _createMaze (){
+		int back;
+		int move;
+		string possibleDirections;
+		Vector2 pos = _start;
+
+		_moves = new List<int>();
+		//_moves.Ad
+		_moves.Add((int)(pos.y + (pos.x * _width)));
+		
+		while ( _moves.Count > 0 )
+		{
+			possibleDirections = "";
+			
+			if (((int)pos.x + 2 < _height ) && (_maze[(int)pos.x + 2][(int)pos.y] == true) && ((int)pos.x + 2 != null) && ((int)pos.x + 2 != _height - 1) )
+			{
+				possibleDirections += SOUTH;
+			}
+			
+			if (((int)pos.x - 2 >= 0 ) && (_maze[(int)pos.x - 2][(int)pos.y] == true) && ((int)pos.x - 2 != null) && ((int)pos.x - 2 != _height - 1) )
+			{
+				possibleDirections += NORTH;
+			}
+			
+			if (((int)pos.y - 2 >= 0 ) && (_maze[(int)pos.x][(int)pos.y - 2] == true) && ((int)pos.y - 2 != null) && ((int)pos.y - 2 != _width - 1) )
+			{
+				possibleDirections += WEST;
+			}
+			
+			if (((int)pos.y + 2 < _width ) && (_maze[(int)pos.x][(int)pos.y + 2] == true) && ((int)pos.y + 2 != null) && ((int)pos.y + 2 != _width - 1) )
+			{
+				possibleDirections += EAST;
+			}
+			
+			if ( possibleDirections.Length > 0 )
+			{
+				move = Random.Range(0, possibleDirections.Length);
+
+				//TODO
+				/*if(possibleDirections[move] == NORTH){
+					_maze[pos.x - 2][pos.y] = false;
+					_maze[pos.x - 1][pos.y] = false;
+					pos.x -=2;
+				}*/
+				switch (possibleDirections[move])
+				{
+				case NORTH: 
+					_maze[(int)pos.x - 2][(int)pos.y] = false;
+					_maze[(int)pos.x - 1][(int)pos.y] = false;
+					pos.x -= 2;
+					break;
+					
+				case SOUTH: 
+					_maze[(int)pos.x + 2][(int)pos.y] = false;
+					_maze[(int)pos.x + 1][(int)pos.y] = false;
+					pos.x +=2;
+					break;
+					
+				case WEST: 
+					_maze[(int)pos.x][(int)pos.y - 2] = false;
+					_maze[(int)pos.x][(int)pos.y - 1] = false;
+					pos.y -=2;
+					break;
+					
+				case EAST: 
+					_maze[(int)pos.x][(int)pos.y + 2] = false;
+					_maze[(int)pos.x][(int)pos.y + 1] = false;
+					pos.y +=2;
+					break;        
+				}
+				_moves.Add((int)pos.y + ((int)pos.x * _width));
+			}
+			else
+			{
+				back = _moves[_moves.Count-1];
+				_moves.Remove(_moves.Count-1);
+				pos.x = back / _width;
+				pos.y = back % _width;
+			}
+		}
+	}
+	
+	private void _drawMaze()
+	{
+		GameObject tile;
+		//var tile : Sprite;
+		
+		/*if ( contains(_container) )
+		{
+			removeChild(_container)
+		}*/
+		
+		//_container = new Sprite();
+		//addChild(_container);
+		
+		for ( int x = 0; x < _height; x++ )
+		{
+			for ( int y = 0; y < _width; y++ )
+			{
+				tile = Instantiate(wall) as GameObject;
+				tile.transform.position = new Vector3(x * TILE_SIZE, y * TILE_SIZE, 0);
+				tile.transform.parent = transform;
+				//tile = (_maze[x][y] == true) ? _drawTile(WALL_COLOR) : _drawTile(WALKABLE_COLOR);
+
+			}
+		}
+		
+		//start tile
+		//tile.x = _start.x * TILE_SIZE;
+		//tile.y = _start.y * TILE_SIZE;
+		//_container.addChild(tile);
+		
+		//finish tile
+		/*tile = _drawTile(FINISH_COLOR);
+			tile.x = _finish.x * TILE_SIZE;
+			tile.y = _finish.y * TILE_SIZE;
+			_container.addChild(tile);*/
+	}
+	
+	
+	
+	/*private int _randInt ( int min, int max)
+	{
+		return int((Math.random() * (max - min + 1)) + min);
+	}*/
+
+	// Use this for initialization
+	void Start () {
+		TiledMazeGen();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Script/Maze1.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Maze1 : MonoBehaviour {
+	public int mazeWidth = 5;
+	public int mazeHeight = 5;
+	public GameObject wall;
+	public GameObject floor;
+
+	private List<int> moves = new List<int>();
+
+	private int[][] maze;
+	//private moves;
+	private int _width;
+	private int _height;
+
+	void Start(){
+		_width = (2*mazeWidth)+1;
+		_height = (2*mazeHeight)+1;
+		Generate();
+		DrawMaze();
+		Debug.Log(maze);
+	}
+
+	void Generate(){
+		maze = new int[_width][];
+		for(int i=0; i<_width; i++){
+			maze[i] = new int[_height];
+			for(int j=0; j<_height; j++){
+				maze[i][j] = 1;
+			}
+		}
+
+		int x_pos = 1;
+		int y_pos = 1;
+		maze[x_pos][y_pos] = 0;
+		moves.Add(y_pos+(x_pos*_width));
+		while(moves.Count>0){
+			string possibleDirections = "";
+			//if((maze[x_pos+2][y_pos] == 1) && (x_pos+2!=0) && (x_pos+2!=_height-1)){
+			if((x_pos+2>=0) && (x_pos+2<=_height-1)){
+				if(maze[x_pos+2][y_pos] == 1){
+					possibleDirections += 'S';
+				}
+			}
+			//if((x_pos-2!=_height-1)){
+
+			//	if((maze[x_pos-2][y_pos] == 1) && (x_pos-2!=0)){
+
+			//if((x_pos-2!=_height-1) && (maze[x_pos-2][y_pos] == 1) && (x_pos-2!=0)){
+			if((x_pos-2<=_height-1) && (x_pos-2>=0)){
+				if((maze[x_pos-2][y_pos] == 1)){
+					possibleDirections += 'N';
+				}
+			}
+
+			//if((maze[x_pos][y_pos-2]==1) && (y_pos-2!=0) && (y_pos-2!=_width-1)){
+			if((y_pos-2>=0) && (y_pos-2<=_width-1)){
+				if(maze[x_pos][y_pos-2]==1){
+					possibleDirections += 'W';
+				}
+			}
+			//if((maze[x_pos][y_pos+2]==1) && (y_pos+2!=0) && (y_pos+2!=_width-1)){
+			if((y_pos+2>=0) && (y_pos+2<=_width-1)){
+				if(maze[x_pos][y_pos+2]==1){
+					possibleDirections += 'E';
+				}
+
+			}
+		
+			if(possibleDirections.Length > 0){
+				int move = Random.Range(0, possibleDirections.Length);
+				switch(possibleDirections[move]){
+					case 'N':
+						maze[x_pos-2][y_pos]=0;
+						maze[x_pos-1][y_pos]=0;
+						x_pos -= 2;
+						break;
+					case 'S':
+						maze[x_pos+2][y_pos]=0;
+						maze[x_pos+1][y_pos]=0;
+						x_pos += 2;
+						break;
+					case 'W':
+						maze[x_pos][y_pos-2]=0;
+						maze[x_pos][y_pos-1]=0;
+						y_pos -= 2;
+						break;
+					case 'E':
+						maze[x_pos][y_pos+2]=0;
+						maze[x_pos][y_pos+1]=0;
+						y_pos += 2;
+						break;
+				}
+				moves.Add(y_pos+(x_pos*_width));
+			}else{
+				int back = moves[moves.Count-1];
+				moves.RemoveAt(moves.Count-1);
+				x_pos = Mathf.FloorToInt(back/_width);
+				y_pos = back%_width;
+			}
+		}
+	}
+
+	void DrawMaze(){
+		for(int i=0; i< _width; i++){
+			for(int j=0; j<_height;j++){
+				Debug.Log(maze[i][j]);
+				if(maze[i][j]==1){
+					GameObject go = Instantiate(wall) as GameObject;
+					go.transform.position = new Vector3(i, j, 0);
+					go.transform.parent = transform;
+				}else if(maze[i][j]==0){
+					GameObject go = Instantiate(floor) as GameObject;
+					go.transform.position = new Vector3(i, j, 0);
+					go.transform.parent = transform;
+				}
+
+			}
+		}
+	}
+}
